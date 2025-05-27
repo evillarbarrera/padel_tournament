492a0f0408454bac510aa534ed9f39da2684877e
ejvillarb@falabella.cl <ejvillarb@CLFIFC02FC1BJMD6N.local>
2025-05-27 17:40:42 -0400

    fix

--- db/migrate/20250527171159_remove_tipoinscripcion_id_from_campeonatos.rb
+++ db/migrate/20250527171159_remove_tipoinscripcion_id_from_campeonatos.rb
@@ -1,6 +1,5 @@
 class RemoveTipoinscripcionIdFromCampeonatos < ActiveRecord::Migration[8.0]
   def change
-    remove_foreign_key :campeonatos, column: :tipoinscripcion_id
     remove_column :campeonatos, :tipoinscripcion_id, :integer
   end
 end
