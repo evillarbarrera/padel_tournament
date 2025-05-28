b8660763dc105393fe1cadbec637d9d162c50148
ejvillarb@falabella.cl <ejvillarb@CLFIFC02FC1BJMD6N.local>
2025-05-28 13:58:36 -0400

    mg

--- db/migrate/20250528175549_add_active_job_id_to_solid_queue_jobs.rb
+++ db/migrate/20250528175549_add_active_job_id_to_solid_queue_jobs.rb
@@ -1,5 +1,6 @@
-class AddActiveJobIdToSolidQueueJobs < ActiveRecord::Migration[8.0]
+class AddMissingColumnsToSolidQueueJobs < ActiveRecord::Migration[7.0]
   def change
     add_column :solid_queue_jobs, :active_job_id, :string
+    add_column :solid_queue_jobs, :priority, :integer, default: 0
   end
 end
