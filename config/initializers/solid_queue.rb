46fe435d763c3dc2f4c4e5737ba1c3af8b802f4d
ejvillarb@falabella.cl <ejvillarb@CLFIFC02FC1BJMD6N.local>
2025-05-28 14:05:20 -0400

    fix

--- config/initializers/solid_queue.rb
+++ config/initializers/solid_queue.rb
@@ -1,3 +1,11 @@
 # config/initializers/solid_queue.rb
 
 SolidQueue::Record.connects_to database: { writing: :primary } if defined?(SolidQueue::Record)
+
+module SolidQueue
+  class Job
+    def priority=(_value)
+      # No hace nada, simplemente ignorar
+    end
+  end
+end
