715ff2a4f91775df4969bca7923029d61ff3d769
ejvillarb@falabella.cl <ejvillarb@CLFIFC02FC1BJMD6N.local>
2025-06-06 14:27:07 -0400

    cambio diseno torneos

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,6 +5,8 @@ class HomeController < ApplicationController
 
   def publicos
     @campeonatos = Campeonato.includes(:club).all
+    @campeonatos = @campeonatos.where(club_id: params[:club_id]) if params[:club_id].present?
+    @campeonatos = @campeonatos.where(tipo: params[:tipo]) if params[:tipo].present?
   end
   
 end
